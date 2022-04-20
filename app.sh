9c16a9fa47c70bb9a58cd85d6ac4a00ce89a8c9f
Mats Sjöberg <mats.sjoberg@csc.fi>
2022-04-20 16:37:30 +0300

    Add fetch models

--- app.sh
+++ app.sh
@@ -4,4 +4,5 @@ export TNPP_PIPELINE=parse_plaintext
 export TNPP_PORT=8080
 export TNPP_MAX_CHARS=15000 # cut-off on character count to parse; protects from too large requests from web
 export FLASK_APP=tnpp_serve
+python3 fetch_models.py fi_tdt_dia
 flask run --host 0.0.0.0 --port $TNPP_PORT
